b94aaec97b14c4858e442d696cd6fc53ce57ef97
WilmarDeML <carewaz23@hotmail.com>
2025-05-08 22:26:58 -0500

    Complete Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello and welcome!");
+        MainService.showHeader();
+        try {
+            ExchangeService.getRatesByBaseCode();
+            if (ExchangeService.getRates() != null) {
+                MainService.runCurrencyConverter();
+            }
+            MainService.exitMessage();
+        } catch (IOException | InterruptedException e) {
+            System.out.println("Error en el servidor de la API: " + e.getLocalizedMessage());
+        }
     }
 }
